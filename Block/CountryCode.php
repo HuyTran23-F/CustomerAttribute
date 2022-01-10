9b1ce71eda2fa899a2c901f83dfdebb54e1aa685
Anh Huy <huykingnice@gmail.com>
2022-01-10 15:44:30 +0700

    customerss

---
+++ Block/CountryCode.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AHT\Customer\Block;
+
+class CountryCode extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @param \Magento\Directory\Model\CountryFactory
+     */
+    protected $_countryFactory;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Directory\Model\CountryFactory $countryFactory,
+        array $data = []
+    ) {
+        $this->_countryFactory = $countryFactory;
+        parent::__construct($context, $data);
+    }
+
+    public function getCountryName($phoneNumber)
+    {
+        $country = $this->_countryFactory->create()->loadByCode($phoneNumber);
+        return $country->getName();
+    }
+}
